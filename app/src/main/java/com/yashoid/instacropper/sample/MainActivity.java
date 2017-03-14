8322ced34191c2e080dfb3916c09741e5664f1f2
Yashar PourMohammad <yasharpm@gmail.com>
2017-03-14 12:26:47 +0330

    Switched photo selection from gallery to camera.

--- app/src/main/java/com/yashoid/instacropper/sample/MainActivity.java
+++ app/src/main/java/com/yashoid/instacropper/sample/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -28,9 +29,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pickPhoto(View v) {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        Intent intent = new Intent(Intent.ACTION_PICK);
+//        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//
+//        startActivityForResult(intent, 1);
 
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getFile()));
         startActivityForResult(intent, 1);
     }
 
@@ -41,17 +46,16 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case 1:
                 if (resultCode == RESULT_OK) {
-                    Intent intent = InstaCropperActivity.getIntent(this, data.getData(), Uri.fromFile(new File(getExternalCacheDir(), "test.jpg")), 720, 50);
+//                    Intent intent = InstaCropperActivity.getIntent(this, data.getData(), Uri.fromFile(new File(getExternalCacheDir(), "test.jpg")), 720, 50);
+                    Intent intent = InstaCropperActivity.getIntent(this, Uri.fromFile(getFile()), Uri.fromFile(getFile()), 720, 50);
                     startActivityForResult(intent, 2);
                 }
                 return;
             case 2:
                 if (resultCode == RESULT_OK) {
-                    mInstaCropper.setImageUri((Uri) data.getData());
+                    mInstaCropper.setImageUri(data.getData());
                 }
                 return;
-        }
-        if (resultCode == RESULT_OK) {
         }
     }
 
@@ -60,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onBitmapReady(Bitmap bitmap) {
-                File file = new File(getExternalCacheDir(), "test.jpg");
+                File file = getFile();
 
                 try {
                     bitmap.compress(Bitmap.CompressFormat.JPEG, 50, new FileOutputStream(file));
@@ -70,6 +74,10 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    private File getFile() {
+        return new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "instaCropper.jpg");
     }
 
 }
